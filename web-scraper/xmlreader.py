d8ad44755cf530dc3ee58743d0182827e0d40292
boredomxd <zizhao.h001@gmail.com>
2021-12-30 21:47:34 -0800

    Slight cleanup.

--- web-scraper/xmlreader.py
+++ web-scraper/xmlreader.py
@@ -7,19 +7,14 @@ import sys
 ### reads in the xml and creates a tree for parsing ###
 url = 'https://politepol.com/fd/2DbU8Ei8mH9F'
 response = urllib.request.urlopen(url)
-tree = ET.fromstring(response.read())
+data = response.read()
 response.close()
 
-
-### reads in the xml for direct mapping to json ###
-file = urllib.request.urlopen('https://politepol.com/fd/2DbU8Ei8mH9F')
-data = file.read()
-file.close()
-dataStr = xmltodict.parse(data)
-
 # Writes data to file for testing.
+dataStr = xmltodict.parse(data)
 with open("Output.txt", "w") as text_file:
     text_file.write(json.dumps(dataStr))
+
 
 # ### retrieves title ###
 # for compTitle in tree.findall('.//title'):
