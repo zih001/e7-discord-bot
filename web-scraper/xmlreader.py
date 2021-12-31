79fff0965b888000b5be99313e5239f70ab7b342
zih001 <zizhao.h001@gmail.com>
2021-12-30 21:49:15 -0800

    Feature/write json to file (#2)
    Added local output test file logic

--- web-scraper/xmlreader.py
+++ web-scraper/xmlreader.py
@@ -7,30 +7,24 @@ import sys
 ### reads in the xml and creates a tree for parsing ###
 url = 'https://politepol.com/fd/2DbU8Ei8mH9F'
 response = urllib.request.urlopen(url)
-tree = ET.fromstring(response.read())
+data = response.read()
 response.close()
 
+# Writes data to file for testing.
+dataStr = xmltodict.parse(data)
+with open("Output.txt", "w") as text_file:
+    text_file.write(json.dumps(dataStr))
 
-### reads in the xml for direct mapping to json ###
-file = urllib.request.urlopen('https://politepol.com/fd/2DbU8Ei8mH9F')
-data = file.read()
-data = xmltodict.parse(data)
-### check to ensure all data got read ###
-# text = json.dumps(data)
-# with open('check', 'w') as f:
-#     f.write(text)
-# f.closed
-file.close()
 
-### retrieves title ###
-for compTitle in tree.findall('.//title'):
-     print(compTitle.text)
+# ### retrieves title ###
+# for compTitle in tree.findall('.//title'):
+#      print(compTitle.text)
 
-### retrieves published date ###
-for compPubDate in tree.findall('.//pubDate'):
-     print(compPubDate.text)
+# ### retrieves published date ###
+# for compPubDate in tree.findall('.//pubDate'):
+#      print(compPubDate.text)
 
-### retrieves hyperlinks ###
-for compLink in tree.findall('.//link'):
-     print(compLink.text)
+# ### retrieves hyperlinks ###
+# for compLink in tree.findall('.//link'):
+#      print(compLink.text)
 
