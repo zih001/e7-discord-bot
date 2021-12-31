3645123f88ea11492871e7dae0a2f968d0b71aca
boredomxd <zizhao.h001@gmail.com>
2021-12-31 13:21:18 -0800

    Extracted post list from data, and finding pubDates

--- web-scraper/xmlreader.py
+++ web-scraper/xmlreader.py
@@ -3,6 +3,7 @@ import urllib.request
 import xmltodict
 import json
 import sys
+#import pdb; 
 
 ### reads in the xml and creates a tree for parsing ###
 url = 'https://politepol.com/fd/2DbU8Ei8mH9F'
@@ -11,9 +12,23 @@ data = response.read()
 response.close()
 
 # Writes data to file for testing.
-dataStr = xmltodict.parse(data)
+dataDict = xmltodict.parse(data) #dataDict is a OrderedDict obj
 with open("Output.txt", "w") as text_file:
-    text_file.write(json.dumps(dataStr))
+    text_file.write(json.dumps(dataDict))
+
+# Get item (forum posts) array out of data.
+# pdb.set_trace()
+
+# Posts is a List object
+posts = dataDict['rss']['channel']['item']
+
+# iterating over the ordereddict
+for post in posts:
+    print(post['pubDate'])
+
+# TODO 
+# get latest post from mongodb, compare against pubDates, 
+# and trim from the posts obj where pubDate is less than latest date from db
 
 
 # ### retrieves title ###
